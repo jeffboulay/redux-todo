5e8a5ccac60d7c43cdec3f76ae4d47fc403a04de
Jeff Boulay <BoulayJ@DNB.com>
2017-10-01 11:18:24 -0500

    Initial commit

---
+++ src/todo.spec.js
@@ -0,0 +1,61 @@
+import deepFreeze from 'deepfreeze';
+import expect from 'expect'
+
+const testAddTodo = () => {
+    const stateBefore = [];
+    const action = {
+        type: 'ADD_TODO',
+        id: 0,
+        text: 'Learn Redux'
+    };
+    const stateAfter = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        }
+    ];
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+    expect(
+        todos(stateBefore, action)).toEqual(stateAfter);
+};
+
+const testToggleTodo = () => {
+    const stateBefore = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Learn Redux',
+            completed: false
+        }
+    ];
+    const action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+    };
+    const stateAfter = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Learn Redux',
+            completed: true
+        }
+    ];
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+    expect(
+        todos(stateBefore, action)).toEqual(stateAfter);
+};
+testAddTodo();
+testToggleTodo();
